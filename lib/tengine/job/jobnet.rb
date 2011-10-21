4b43fd3add17775fd60cc9fb29080fbd24aa03d3
akima <akima@nautilus-technologies.com>
2011-10-21 18:20:01 +0900

    [ジョブネット制御ドライバの実装(正常系)] Tengine::Job::Jobnetにあった実行時にしか使われないメソッドをTengine::Job::JobnetActualに移動しました

--- lib/tengine/job/jobnet.rb
+++ lib/tengine/job/jobnet.rb
@@ -28,18 +28,6 @@ class Tengine::Job::Jobnet < Tengine::Job::Job
 
   embeds_many :edges, :class_name => "Tengine::Job::Edge", :inverse_of => :owner
 
-  # https://cacoo.com/diagrams/hdLgrzYsTBBpV3Wj#D26C1
-  STATE_TRANSITION_METHODS = [:transmit, :activate, :ack, :succeed, :fail].freeze
-  STATE_TRANSITION_METHODS.each do |method_name|
-    class_eval(<<-END_OF_METHOD)
-      def #{method_name}(signal)
-        script_executable? ?
-          job_#{method_name}(signal) :
-          jobnet_#{method_name}(signal)
-      end
-    END_OF_METHOD
-  end
-
   class << self
     def by_name(name)
       first(:conditions => {:name => name})
--- lib/tengine/job/jobnet_actual.rb
+++ lib/tengine/job/jobnet_actual.rb
@@ -10,4 +10,17 @@ class Tengine::Job::JobnetActual < Tengine::Job::Jobnet
   include Tengine::Job::Jobnet::JobnetStateTransition
 
   field :was_expansion, :type => Boolean # テンプレートがTenigne::Job::Expansionであった場合にtrueです。
+
+  # https://cacoo.com/diagrams/hdLgrzYsTBBpV3Wj#D26C1
+  STATE_TRANSITION_METHODS = [:transmit, :activate, :ack, :succeed, :fail].freeze
+  STATE_TRANSITION_METHODS.each do |method_name|
+    class_eval(<<-END_OF_METHOD)
+      def #{method_name}(signal)
+        script_executable? ?
+          job_#{method_name}(signal) :
+          jobnet_#{method_name}(signal)
+      end
+    END_OF_METHOD
+  end
+
 end
