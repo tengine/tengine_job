9cf2a7b8dab075cd2ece4f423a8d1ddd577ab0da
shyouhei <shyouhei@ruby-lang.org>
2011-11-17 22:14:51 +0900

    tengine/resource/net_ssh のAPIを使います

--- lib/tengine/job/script_executable.rb
+++ lib/tengine/job/script_executable.rb
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 require 'tengine/job'
+require 'tengine/resource/net_ssh'
 
 # ジョブとして実際にスクリプトを実行する処理をまとめるモジュール。
 # Tengine::Job::JobnetActualと、Tengine::Job::ScriptActualがincludeします
@@ -26,7 +27,8 @@ module Tengine::Job::ScriptExecutable
   def execute(cmd)
     raise "actual_server not found for #{self.name_path.inspect}" unless actual_server
     Tengine.logger.info("connecting to #{actual_server.hostname_or_ipv4}")
-    actual_credential.connect(actual_server.hostname_or_ipv4) do |ssh|
+    port = actual_server.properties["ssh_port"] || 22
+    Net::SSH.start(actual_server.hostname_or_ipv4, actual_credential, :port => port, :logger => Tengine.logger, :keys_only => true) do |ssh|
       # see http://net-ssh.github.com/ssh/v2/api/classes/Net/SSH/Connection/Channel.html
       ssh.open_channel do |channel|
         Tengine.logger.info("now exec on ssh: " << cmd)
