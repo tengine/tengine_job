f5343f000e1ae19f32ea3ab45aafd2ca699fb67a
akima <akima@nautilus-technologies.com>
2011-12-12 11:24:33 +0900

    [expansionで読み込むジョブネットが複数のバージョンがあるときに、Tengine::Job::RootJobnetTemplateのdsl_versionが考慮されず、古いバージョンのジョブネットを利用してしまう] インデントを整えました

--- spec/tengine/job/drivers/job_controll_driver/expansion_spec.rb
+++ spec/tengine/job/drivers/job_controll_driver/expansion_spec.rb
@@ -9,48 +9,46 @@ describe 'job_control_driver' do
   driver :job_control_driver
 
   shared_examples_for "/rjn0008/rjn0001/j11を実行する際の環境変数" do |dsl_version|
-
-      it "expansionだったジョブネットよりも上位のジョブの情報は出力されない" do
-        @rjn0001 = @root.vertex_by_name_path("/rjn0008/rjn0001")
-        @j11 = @root.vertex_by_name_path("/rjn0008/rjn0001/j11")
-        @root.phase_key = :running
-        @rjn0001.phase_key = :running
-        @j11.phase_key = :ready
-        @j11.prev_edges.each{|edge| edge.phase_key = :transmitting}
-        @root.save!
-        @root.reload
-        tengine.should_not_fire
-        mock_ssh = mock(:ssh)
-        mock_channel = mock(:channel)
-        Net::SSH.should_receive(:start).
-          with("localhost", an_instance_of(Tengine::Resource::Credential), an_instance_of(Hash)).and_yield(mock_ssh)
-        mock_ssh.should_receive(:open_channel).and_yield(mock_channel)
-        mock_channel.should_receive(:exec) do |*args|
-          args.length.should == 1
-          # args.first.should =~ %r<source \/etc\/profile && export MM_ACTUAL_JOB_ID=[0-9a-f]{24} MM_ACTUAL_JOB_ANCESTOR_IDS=\\"[0-9a-f]{24}\\" MM_FULL_ACTUAL_JOB_ANCESTOR_IDS=\\"[0-9a-f]{24}\\" MM_ACTUAL_JOB_NAME_PATH=\\"/rjn0001/j11\\" MM_ACTUAL_JOB_SECURITY_TOKEN= MM_SCHEDULE_ID=[0-9a-f]{24} MM_SCHEDULE_ESTIMATED_TIME= MM_TEMPLATE_JOB_ID=[0-9a-f]{24} MM_TEMPLATE_JOB_ANCESTOR_IDS=\\"[0-9a-f]{24}\\" && tengine_job_agent_run -- \$HOME/j11\.sh>
-          args.first.should =~ %r<source \/etc\/profile>
+    it "expansionだったジョブネットよりも上位のジョブの情報は出力されない" do
+      @rjn0001 = @root.vertex_by_name_path("/rjn0008/rjn0001")
+      @j11 = @root.vertex_by_name_path("/rjn0008/rjn0001/j11")
+      @root.phase_key = :running
+      @rjn0001.phase_key = :running
+      @j11.phase_key = :ready
+      @j11.prev_edges.each{|edge| edge.phase_key = :transmitting}
+      @root.save!
+      @root.reload
+      tengine.should_not_fire
+      mock_ssh = mock(:ssh)
+      mock_channel = mock(:channel)
+      Net::SSH.should_receive(:start).
+        with("localhost", an_instance_of(Tengine::Resource::Credential), an_instance_of(Hash)).and_yield(mock_ssh)
+      mock_ssh.should_receive(:open_channel).and_yield(mock_channel)
+      mock_channel.should_receive(:exec) do |*args|
+        args.length.should == 1
+        # args.first.should =~ %r<source \/etc\/profile && export MM_ACTUAL_JOB_ID=[0-9a-f]{24} MM_ACTUAL_JOB_ANCESTOR_IDS=\\"[0-9a-f]{24}\\" MM_FULL_ACTUAL_JOB_ANCESTOR_IDS=\\"[0-9a-f]{24}\\" MM_ACTUAL_JOB_NAME_PATH=\\"/rjn0001/j11\\" MM_ACTUAL_JOB_SECURITY_TOKEN= MM_SCHEDULE_ID=[0-9a-f]{24} MM_SCHEDULE_ESTIMATED_TIME= MM_TEMPLATE_JOB_ID=[0-9a-f]{24} MM_TEMPLATE_JOB_ANCESTOR_IDS=\\"[0-9a-f]{24}\\" && tengine_job_agent_run -- \$HOME/j11\.sh>
+        args.first.should =~ %r<source \/etc\/profile>
           t_rjn1001 = Tengine::Job::RootJobnetTemplate.find_by_name("rjn0001")
-          t_rjn1001.dsl_version.should == dsl_version
-          t_j11 = t_rjn1001.vertex_by_name_path("/rjn0001/j11")
-          args.first.should =~ %r<MM_TEMPLATE_JOB_ID=#{t_j11.id.to_s}>
+        t_rjn1001.dsl_version.should == dsl_version
+        t_j11 = t_rjn1001.vertex_by_name_path("/rjn0001/j11")
+        args.first.should =~ %r<MM_TEMPLATE_JOB_ID=#{t_j11.id.to_s}>
           args.first.should_not =~ %r<MM_TEMPLATE_JOB_ANCESTOR_IDS=\"#{@template.id.to_s};#{t_rjn1001.id.to_s}\">
           args.first.should =~ %r<MM_TEMPLATE_JOB_ANCESTOR_IDS=\"#{t_rjn1001.id.to_s}\">
           args.first.should =~ %r<job_test j11>
-        end
-        tengine.receive("start.job.job.tengine", :properties => {
-            :execution_id => @execution.id.to_s,
-            :root_jobnet_id => @root.id.to_s,
-            :target_jobnet_id => @rjn0001.id.to_s,
-            :target_job_id => @j11.id.to_s,
-          })
-        @root.reload
-        @rjn0001 = @root.vertex_by_name_path("/rjn0008/rjn0001")
-        @j11 = @root.vertex_by_name_path("/rjn0008/rjn0001/j11")
-        @root.phase_key = :running
-        @rjn0001.phase_key = :running
       end
-
+      tengine.receive("start.job.job.tengine", :properties => {
+          :execution_id => @execution.id.to_s,
+          :root_jobnet_id => @root.id.to_s,
+          :target_jobnet_id => @rjn0001.id.to_s,
+          :target_job_id => @j11.id.to_s,
+        })
+      @root.reload
+      @rjn0001 = @root.vertex_by_name_path("/rjn0008/rjn0001")
+      @j11 = @root.vertex_by_name_path("/rjn0008/rjn0001/j11")
+      @root.phase_key = :running
+      @rjn0001.phase_key = :running
     end
+  end
 
   # in [rjn0008]
   # (S1) --e1-->(rjn0001)--e2-->(rjn0002)--e3-->(E1)
