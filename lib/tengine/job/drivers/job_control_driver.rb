5974c2a54a0f4da4acf294f2a3281534644cbfb2
akima <akima@nautilus-technologies.com>
2011-12-26 16:33:20 +0900

    [tengindのプロセスを二つ起動した際に並列ジョブがある際にジョブが２度実行される] update_with_lockから複数回実行されうるブロックの中で最初にTengine::Job::Signal#resetするようにしました

--- lib/tengine/job/drivers/job_control_driver.rb
+++ lib/tengine/job/drivers/job_control_driver.rb
@@ -8,6 +8,7 @@ driver :job_control_driver do
     # activate
     root_jobnet = Tengine::Job::RootJobnetActual.find(event[:root_jobnet_id])
     root_jobnet.update_with_lock do
+      signal.reset
       target_jobnet = root_jobnet.find_descendant(event[:target_jobnet_id]) || root_jobnet
       target_job = target_jobnet.find_descendant(event[:target_job_id])
       signal.with_paths_backup do
@@ -30,6 +31,7 @@ driver :job_control_driver do
     signal = Tengine::Job::Signal.new(event)
     root_jobnet = Tengine::Job::RootJobnetActual.find(event[:root_jobnet_id])
     root_jobnet.update_with_lock do
+      signal.reset
       target_jobnet = root_jobnet.find_descendant(event[:target_jobnet_id]) || root_jobnet
       target_job = target_jobnet.find_descendant(event[:target_job_id])
       signal.with_paths_backup do
@@ -44,6 +46,7 @@ driver :job_control_driver do
     root_jobnet = Tengine::Job::RootJobnetActual.find(event[:root_jobnet_id])
     # finish
     root_jobnet.update_with_lock do
+      signal.reset
       job = root_jobnet.find_descendant(event[:target_job_id])
       job.finish(signal)
     end
@@ -64,6 +67,7 @@ driver :job_control_driver do
     signal = Tengine::Job::Signal.new(event)
     root_jobnet = Tengine::Job::RootJobnetActual.find(event[:root_jobnet_id])
     root_jobnet.update_with_lock do
+      signal.reset
       job = root_jobnet.find_descendant(event[:target_job_id])
       job.reset(signal)
       job.transmit(signal)
